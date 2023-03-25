9a5b4519f5c2fa0ecb8f33a44c4c7f3cd44658f3
demogorgon1 <neckelmann@gmail.com>
2023-03-25 20:43:15 +0100

    - Added API.h header.

---
+++ include/kaos-public/API.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Base.h"
+
+#include "Components/Combat.h"
+#include "Components/NPC.h"
+#include "Components/Position.h"
+
+#include "Data/Ability.h"
+#include "Data/Class.h"
+#include "Data/Entity.h"
+#include "Data/Map.h"
+#include "Data/Talent.h"
+#include "Data/TalentTree.h"
+
+#include "Effects/Damage.h"
+#include "Effects/Stun.h"
+
+#include "Systems/Combat.h"
+#include "Systems/NPC.h"
+
+#include "Compiler.h"
+#include "Component.h"
+#include "ComponentBase.h"
+#include "ComponentFactory.h"
+#include "DataBase.h"
+#include "DataErrorHandling.h"
+#include "DataType.h"
+#include "Effect.h"
+#include "EffectBase.h"
+#include "EffectFactory.h"
+#include "EntityInstance.h"
+#include "Manifest.h"
+#include "Parser.h"
+#include "PersistentIdTable.h"
+#include "SourceContext.h"
+#include "Stat.h"
+#include "System.h"
+#include "SystemBase.h"
+#include "SystemFactory.h"
+#include "Vec2.h"
+
