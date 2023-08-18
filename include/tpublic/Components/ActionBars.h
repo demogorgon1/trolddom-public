79e0a2a16e22d22ffea27949caba1ebcec00fed2
demogorgon1 <neckelmann@gmail.com>
2023-08-18 14:17:24 +0200

    - Action bars component set method.

--- include/tpublic/Components/ActionBars.h
+++ include/tpublic/Components/ActionBars.h
@@ -16,6 +16,8 @@ namespace tpublic
 			static const uint8_t FLAGS = FLAG_REPLICATE_TO_OWNER | FLAG_PLAYER_ONLY;			
 			static const Persistence::Id PERSISTENCE = Persistence::ID_MAIN;
 
+			static const uint32_t MAX_ROWS = 4;
+
 			ActionBars()
 				: ComponentBase(ID, FLAGS, PERSISTENCE)
 			{
@@ -26,6 +28,19 @@ namespace tpublic
 			~ActionBars()
 			{
 
+			}
+
+			void
+			Set(
+				uint32_t	aRow,
+				uint32_t	aIndex,
+				uint32_t	aAbilityId)
+			{
+				TP_CHECK(aRow < MAX_ROWS, "Invalid action bar row.");
+				TP_CHECK(aIndex < (uint32_t)ActionBar::MAX_SIZE, "Invalid action bar index.");
+				while(m_actionBars.size() < (size_t)aRow + 1)
+					m_actionBars.push_back(std::make_unique<ActionBar>());
+				m_actionBars[aRow]->SetSlot((size_t)aIndex, aAbilityId);
 			}
 
 			// ComponentBase implementation
