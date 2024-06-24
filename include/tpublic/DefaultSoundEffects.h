02900162c4fc17838fe1e22a7f643fa147f17a35
demogorgon1 <neckelmann@gmail.com>
2024-06-24 14:35:37 +0200

    - More sound effects.

--- include/tpublic/DefaultSoundEffects.h
+++ include/tpublic/DefaultSoundEffects.h
@@ -20,10 +20,8 @@ namespace tpublic
 			{
 				if(aChild->m_name == "melee")
 					m_melee.FromSource(aChild);
-				else if (aChild->m_name == "ranged")
-					m_ranged.FromSource(aChild);
-				else if (aChild->m_name == "spell")
-					m_spell.FromSource(aChild);
+				else if (aChild->m_name == "offensive_spell")
+					m_offensiveSpell.FromSource(aChild);
 				else if (aChild->m_name == "misc")
 					m_misc.FromSource(aChild);
 				else if(aChild->m_name == "consumable")
@@ -38,8 +36,7 @@ namespace tpublic
 			IWriter*						aStream) const 
 		{
 			m_melee.ToStream(aStream);
-			m_ranged.ToStream(aStream);
-			m_spell.ToStream(aStream);
+			m_offensiveSpell.ToStream(aStream);
 			m_misc.ToStream(aStream);
 			aStream->WriteUInt(m_consumableSoundId);				
 		}
@@ -50,9 +47,7 @@ namespace tpublic
 		{
 			if (!m_melee.FromStream(aStream))
 				return false;
-			if (!m_ranged.FromStream(aStream))
-				return false;
-			if (!m_spell.FromStream(aStream))
+			if (!m_offensiveSpell.FromStream(aStream))
 				return false;
 			if (!m_misc.FromStream(aStream))
 				return false;
@@ -63,8 +58,7 @@ namespace tpublic
 				
 		// Public data
 		SoundEffect::Collection	m_melee;
-		SoundEffect::Collection	m_ranged;
-		SoundEffect::Collection	m_spell;
+		SoundEffect::Collection	m_offensiveSpell;
 		SoundEffect::Collection	m_misc;
 		uint32_t				m_consumableSoundId = 0;
 	};
