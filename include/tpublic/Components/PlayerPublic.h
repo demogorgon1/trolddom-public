5fc5ac3431ad257ffc243ea291c619b1a2ba5e14
demogorgon1 <neckelmann@gmail.com>
2024-10-04 17:06:45 +0200

    - Player status (AFK/DND).

--- include/tpublic/Components/PlayerPublic.h
+++ include/tpublic/Components/PlayerPublic.h
@@ -19,11 +19,19 @@ namespace tpublic
 			static const Persistence::Id PERSISTENCE = Persistence::ID_NONE;
 			static const Replication REPLICATION = REPLICATION_PUBLIC;
 
+			enum Status : uint8_t
+			{
+				STATUS_NORMAL,
+				STATUS_AFK,
+				STATUS_DND
+			};
+
 			enum Field
 			{
 				FIELD_CLASS_ID,
 				FIELD_CHARACTER_ID,
-				FIELD_STATS
+				FIELD_STATS,
+				FIELD_STATUS
 			};
 
 			static void
@@ -33,6 +41,7 @@ namespace tpublic
 				aSchema->Define(ComponentSchema::TYPE_UINT32, FIELD_CLASS_ID, NULL, offsetof(PlayerPublic, m_classId));
 				aSchema->Define(ComponentSchema::TYPE_UINT32, FIELD_CHARACTER_ID, NULL, offsetof(PlayerPublic, m_characterId));
 				aSchema->DefineCustomObjectNoSource<Stat::Collection>(FIELD_STATS, offsetof(PlayerPublic, m_stats));
+				aSchema->DefineCustomPODNoSource<Status>(FIELD_STATUS, offsetof(PlayerPublic, m_status));
 			}
 
 			void
@@ -41,12 +50,14 @@ namespace tpublic
 				m_classId = 0;
 				m_characterId = 0;
 				m_stats.Reset();
+				m_status = STATUS_NORMAL;
 			}
 
 			// Public data
 			uint32_t			m_classId = 0;
 			uint32_t			m_characterId = 0;
 			Stat::Collection	m_stats;			
+			Status				m_status = STATUS_NORMAL;
 		};
 	}
 
