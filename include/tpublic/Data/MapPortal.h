0e42b19396e39a9b17ffb1bfb45e8240cad4c325
demogorgon1 <neckelmann@gmail.com>
2024-10-29 10:31:11 +0100

    - Portal animations.

--- include/tpublic/Data/MapPortal.h
+++ include/tpublic/Data/MapPortal.h
@@ -14,6 +14,58 @@ namespace tpublic
 			static const DataType::Id DATA_TYPE = DataType::ID_MAP_PORTAL;
 			static const bool TAGGED = false;
 
+			struct Animation
+			{
+				Animation()
+				{
+
+				}
+
+				Animation(
+					const SourceNode*	aSource)
+				{
+					aSource->GetObject()->ForEachChild([&](
+						const SourceNode* aChild)
+					{
+						if(aChild->m_name == "frames")
+							aChild->GetIdArray(DataType::ID_SPRITE, m_frames);
+						else if(aChild->m_name == "frame_interval")
+							m_frameInterval = aChild->GetUInt32();
+						else if(aChild->m_name == "color")
+							m_color = Image::RGBA(aChild);
+						else
+							TP_VERIFY(false, aChild->m_debugInfo, "'%s' is not a valid item.", aChild->GetIdentifier());
+					});
+				}
+
+				void
+				ToStream(
+					IWriter*			aWriter) const
+				{
+					aWriter->WriteUInts(m_frames);
+					aWriter->WriteUInt(m_frameInterval);
+					aWriter->WritePOD(m_color);
+				}
+
+				bool
+				FromStream(
+					IReader*			aReader)
+				{
+					if(!aReader->ReadUInts(m_frames))
+						return false;
+					if(!aReader->ReadUInt(m_frameInterval))
+						return false;
+					if (!aReader->ReadPOD(m_color))
+						return false;
+					return true;
+				}
+
+				// Public data
+				std::vector<uint32_t>		m_frames;
+				uint32_t					m_frameInterval = 100;
+				Image::RGBA					m_color = { 255, 255, 255, 255 };
+			};
+
 			void
 			Verify() const
 			{
@@ -34,6 +86,8 @@ namespace tpublic
 							m_mapId = aChild->m_sourceContext->m_persistentIdTable->GetId(DataType::ID_MAP, aChild->GetIdentifier());
 						else if (aChild->m_name == "spawn")
 							m_mapPlayerSpawnId = aChild->m_sourceContext->m_persistentIdTable->GetId(DataType::ID_MAP_PLAYER_SPAWN, aChild->GetIdentifier());
+						else if(aChild->m_name == "animation")
+							m_animation = Animation(aChild);
 						else
 							TP_VERIFY(false, aChild->m_debugInfo, "'%s' is not a valid item.", aChild->m_name.c_str());
 					}
@@ -46,6 +100,7 @@ namespace tpublic
 			{
 				aStream->WriteUInt(m_mapId);
 				aStream->WriteUInt(m_mapPlayerSpawnId);
+				aStream->WriteOptionalObject(m_animation);
 			}
 
 			bool
@@ -56,12 +111,15 @@ namespace tpublic
 					return false;
 				if (!aStream->ReadUInt(m_mapPlayerSpawnId))
 					return false;
+				if(!aStream->ReadOptionalObject(m_animation))
+					return false;
 				return true;
 			}
 
 			// Public data
-			uint32_t		m_mapId = 0;
-			uint32_t		m_mapPlayerSpawnId = 0;
+			uint32_t					m_mapId = 0;
+			uint32_t					m_mapPlayerSpawnId = 0;
+			std::optional<Animation>	m_animation;
 		};
 
 	}
