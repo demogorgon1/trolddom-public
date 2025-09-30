d0d211dfbef581245dac46bd743c3ed57957aa76
demogorgon1 <neckelmann@gmail.com>
2025-09-30 15:26:21 +0200

    - Quests and a new mount.

--- include/tpublic/Data/Mount.h
+++ include/tpublic/Data/Mount.h
@@ -34,6 +34,8 @@ namespace tpublic
 							m_spriteId = aChild->GetId(DataType::ID_SPRITE);
 						else if (aChild->m_name == "walk_sprites")
 							aChild->GetIdArray(DataType::ID_SPRITE, m_walkSpriteIds);
+						else if(aChild->m_name == "y_offset")
+							m_yOffset = aChild->GetInt32();
 						else
 							TP_VERIFY(false, aChild->m_debugInfo, "'%s' is not a valid item.", aChild->m_name.c_str());
 					}
@@ -46,6 +48,7 @@ namespace tpublic
 			{
 				aStream->WriteUInt(m_spriteId);
 				aStream->WriteUInts(m_walkSpriteIds);
+				aStream->WriteInt(m_yOffset);
 			}
 
 			bool
@@ -56,12 +59,15 @@ namespace tpublic
 					return false;
 				if (!aStream->ReadUInts(m_walkSpriteIds))
 					return false;
+				if (!aStream->ReadInt(m_yOffset))
+					return false;
 				return true;
 			}
 
 			// Public data
 			uint32_t				m_spriteId = 0;
 			std::vector<uint32_t>	m_walkSpriteIds;
+			int32_t					m_yOffset = 0;
 		};
 
 
