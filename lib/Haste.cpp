7120f9e78ab95599865f6c500cb729042069933a
demogorgon1 <neckelmann@gmail.com>
2025-12-23 16:59:03 +0100

    - Haste has diminishing returns. - Reduced mana cost of mana Avenger abilities. - New priest dragon talent and ability: Counterkick. - Increased damage of Dragon Crush. - Cooldown of Pain and Suffering reduced by 1 second. - Target dummies are level 30. - Off-hand damage increased from 50% to 70%.

--- lib/Haste.cpp
+++ lib/Haste.cpp
@@ -12,7 +12,12 @@ namespace tpublic
 			int32_t						aBaseCastTime,
 			float						aHaste)
 		{
-			return (int32_t)((float)aBaseCastTime * (1.0f - aHaste / 100.0f));
+			//return (int32_t)((float)aBaseCastTime * (1.0f - aHaste / 100.0f));
+
+			float x = aHaste / 100.0f;
+			float y = 0.5892858f * x + 1.2857139f * x * x - 1.053f * x * x * x; // Fitted 3rd degree polynomial for smooth diminishing return after ~50%
+
+			return (int32_t)((float)aBaseCastTime * (1.0f - y));
 		}
 
 	}
